a622e2a3d898745b7635cc7d86badbaff38d19ef
lizzieeli <lizzieli1201@gmail.com>
2025-03-23 17:47:59 -0500

    adding more to dimm cpp simulation

---
+++ tb/l1d_llc_tb.sv
@@ -0,0 +1,14 @@
+// tb meant to ensure connection between caches work
+module l1d_llc_tb (
+    // Input signals
+    input logic clk,
+    input logic rst_N,
+
+    // L1D cache interface inputs
+    
+    // L1D cache interface outputs
+
+);
+
+
+endmodule
